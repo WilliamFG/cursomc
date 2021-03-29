beb592cd358cfb50075537115a0f02454889f0a3
WilliamFG <william.goncalves22@gmail.com>
2021-03-28 22:57:42 -0300

    Criando repository e service para classe Categoria

--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.example.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable { /* Classe */
 	private static final long serialVersionUID = 1L;
 	
 	/* Atributos */
+	@Id
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,31 @@ package com.example.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias") /* Mapeia o endpoint no navegador */
 public class CategoriaResource {
+	
+	@Autowired /* instância objetos automaticamente */
+	public CategoriaService service;
+	
+	/* método GET para tratar as requisições feitas em /categoria */
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET) 
+	
+	/* @PathVariable para conectar o id da requisição com id da classe */
+	public ResponseEntity<?> find(@PathVariable Integer id) { 
+	Categoria obj = service.buscar(id);
+	return ResponseEntity.ok().body(obj);	
 		
-	@RequestMapping(method = RequestMethod.GET) /* método GET para tratar as requisições feitas em /categoria */
-	public List<Categoria> listar () { 
-		
-		/* Instância dois objetos do tipo categoria e inclui em uma List */
-		
-		Categoria cat1 = new Categoria(1, "Informática"); 
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista =  new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista ;
 	}
 
 }
